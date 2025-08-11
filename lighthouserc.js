2e203e420b225238998ae3636370a715d3db56cb
Hayder Alraeai <alraeai.hayder@gmail.com>
2025-08-11 12:31:28 +0200

    Refactor Lighthouse CI workflow and restore configuration file

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        "https://tre.se/",
+        "https://www.tre.se/handla/mobiltelefoner",
+        "https://www.tre.se/support",
+      ],
+    },
+  },
+}
